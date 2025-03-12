ce1270b1844c50e05b026e7bd2aa42910d5b7f4a
samiul2102013 <samiulhasan0186@gmail.com>
2025-03-12 12:06:16 +0600

    media file settings done and product model created

--- SamiulMart/urls.py
+++ SamiulMart/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +12,5 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
     path('store/', include('store.urls')),
     #path('category/', include('category.urls')),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
