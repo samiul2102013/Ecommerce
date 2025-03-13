dee690e22c350514d33afae04bc40a07162c05c6
samiul2102013 <samiulhasan0186@gmail.com>
2025-03-14 00:25:30 +0600

    category filtering and pagination added

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.store, name='store'),
+    path('category/<slug:category_slug>', views.store, name='product_by_category'),
     path('product_detail/', views.product_detail, name='product_detail'),
     
-]
+]   
--- store/views.py
+++ store/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def store(request):
-    return render(request, 'store/store.html')
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from category.models import Category
+from django.core.paginator import Paginator
+ 
+def store(request, category_slug=None):
+    catetory = None
+    products = None
+    if category_slug:
+        category = get_object_or_404(Category, slug = category_slug)
+        products = Product.objects.filter(is_available=True, category = category)
+        paginator = Paginator(products, 1)  #paginator(object, per page e koita object rakhte cai)
+        page = request.GET.get('page')
+        paged_product=paginator.get_page(page)
+    else:
+        products = Product.objects.filter(is_available=True)
+        paginator = Paginator(products,2)
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
+        
+    categories = Category.objects.all()
+    context = {'products':paged_product, 'categories':categories}
+    return render(request, 'store/store.html', context)
 
 def product_detail(request):
     return render(request, 'store/product-detail.html')
